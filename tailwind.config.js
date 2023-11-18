18e817b7383299f128732d28543fb029e2784cf0
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2023-11-18 22:21:58 +0700

    fix: remove unused tailwind files

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  theme: {
-    // Some useful comment
-    fontFamily: {
-      nunito: ["nunito", "sans-serif"],
-      MyFont: ['"My Font"', "serif"], // Ensure fonts with spaces have " " surrounding it.
-    },
-  },
-};
