3cf16ce10e2f4b8502d71ab9c856b083d039be8d
SarutaBamboo <65070211@kmitl.ac.th>
2023-11-18 20:48:33 +0700

    storaat

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ const silaProfileCard = document.getElementById("sila-profile-card");
 const sarutaProfileCard = document.getElementById("saruta-profile-card");
 
 const showPortfolioPageById = (username) => {
-    window.location.href = `${username}/${username}.html`
-}
+  window.location.href = `${username}/${username}.html`;
+};
 
-silePortButton.addEventListener('click', () => showPortfolioPageById("sipakdee"))
-silaProfileCard.addEventListener('click', () => showPortfolioPageById("sipakdee"))
-surutaPortButton.addEventListener('click', () => showPortfolioPageById("storaat"))
-sarutaProfileCard.addEventListener('click', () => showPortfolioPageById("storaat"))
+silePortButton.addEventListener("click", () =>
+  showPortfolioPageById("sipakdee")
+);
+silaProfileCard.addEventListener("click", () =>
+  showPortfolioPageById("sipakdee")
+);
+surutaPortButton.addEventListener("click", () =>
+  showPortfolioPageById("storaat")
+);
+sarutaProfileCard.addEventListener("click", () =>
+  showPortfolioPageById("storaat")
+);
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  theme: {
+    // Some useful comment
+    fontFamily: {
+      nunito: ["nunito", "sans-serif"],
+      MyFont: ['"My Font"', "serif"], // Ensure fonts with spaces have " " surrounding it.
+    },
+  },
+};
